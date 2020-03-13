b9b572c7bd0683b9c42002e7775308367826fef3
Johnny <syonip@gmail.com>
2020-03-13 21:26:18 +0200

    added firebase storage

---
+++ lib/apis/encoding_provider.dart
@@ -0,0 +1,165 @@
+import 'dart:io';
+import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
+
+removeExtension(String path) {
+  final str = path.substring(0, path.length - 4);
+  return str;
+}
+
+class EncodingProvider {
+  static final FlutterFFmpeg _encoder = FlutterFFmpeg();
+  static final FlutterFFprobe _probe = FlutterFFprobe();
+  static final FlutterFFmpegConfig _config = FlutterFFmpegConfig();
+
+  static Future<String> encode(videoPath) async {
+    if (!File(videoPath).existsSync())
+      throw Exception("File at path: $videoPath doesn't exist.");
+
+    final noExt = removeExtension(videoPath);
+    final String outPath = '$noExt-encoded.mp4';
+    List<String> arguments = [
+      "-y", // overwrite
+      "-i",
+      videoPath,
+      "-an",
+      "-c:v",
+      "libx264",
+      // "-x265-params",
+      // "lossless=1",
+      // "-crf",
+      // "22",
+      "-preset",
+      "slow",
+      "-b:v",
+      "2M",
+      "-bufsize",
+      "2M",
+      // "-profile:v",
+      // "baseline",
+      outPath
+    ];
+
+    final int rc = await _encoder.executeWithArguments(arguments);
+    if (rc != 0)
+      throw Exception(
+          'Encoding executeWithArguments failed with return code: $rc');
+
+    var outFile = File(outPath);
+    if (!outFile.existsSync()) {
+      throw new Exception("Encoding did not create an output file.");
+    }
+    return outPath;
+  }
+
+  static Future<String> encodeHLS(videoPath, extDir) async {
+    if (!File(videoPath).existsSync())
+      throw Exception("File at path: $videoPath doesn't exist.");
+
+    final outDirPath = '${extDir.path}/Videos/hls';
+    final videosDir = new Directory(outDirPath);
+
+    videosDir.createSync();
+    List<String> arguments = [
+      "-y", // overwrite
+      "-i",
+      videoPath,
+      "-an",
+      "-c:v",
+      "libx264",
+      "-preset",
+      "ultrafast",
+      "-crf",
+      "20",
+      "-g",
+      "48",
+      "-keyint_min",
+      "48",
+      "-sc_threshold",
+      "0",
+      "-b:v",
+      "2500k",
+      "-maxrate",
+      "2675k",
+      "-bufsize",
+      "3750k",
+      "-hls_time",
+      "4",
+      "-hls_playlist_type",
+      "vod",
+      "-hls_segment_filename",
+      "$outDirPath/720p_%03d.ts",
+      "$outDirPath/720p.m3u8",
+    ];
+
+    final int rc = await _encoder.executeWithArguments(arguments);
+    if (rc != 0)
+      throw Exception(
+          'Encoding executeWithArguments failed with return code: $rc');
+
+    return outDirPath;
+  }
+
+  static Future<double> getAspectRatio(videoPath) async {
+    if (!File(videoPath).existsSync())
+      throw Exception("File at path: $videoPath doesn't exist.");
+
+    final info = await _probe.getMediaInformation(videoPath);
+
+    final int width = info["streams"][0]["width"];
+    final int height = info["streams"][0]["height"];
+    final double aspect = height / width;
+    return aspect;
+  }
+
+  static Future<String> getThumb(videoPath, width, height) async {
+    if (!File(videoPath).existsSync())
+      throw Exception("File at path: $videoPath doesn't exist.");
+
+    final String outPath = '$videoPath.jpg';
+    List<String> arguments = [
+      "-y", // overwrite
+      "-i",
+      videoPath,
+      "-vframes",
+      "1",
+      "-an",
+      "-s",
+      "${width}x${height}",
+      "-ss",
+      "1",
+      outPath
+    ];
+    final int rc = await _encoder.executeWithArguments(arguments);
+
+    if (rc != 0)
+      throw Exception(
+          'Generating thumbnail executeWithArguments failed with return code: $rc');
+
+    var outFile = File(outPath);
+    if (!outFile.existsSync()) {
+      throw new Exception(
+          "Generating thumbnail did not create an output file.");
+    }
+
+    return outPath;
+  }
+
+  static void enableStatisticsCallback(Function cb) {
+    return _config.enableStatisticsCallback(cb);
+  }
+
+  static Future<void> cancel() async {
+    await _encoder.cancel();
+  }
+
+  static Future<Map<dynamic, dynamic>> getMediaInformation(String path) async {
+    if (!File(path).existsSync())
+      throw Exception("File at path: $path doesn't exist.");
+
+    return await _probe.getMediaInformation(path);
+  }
+
+  static void enableLogCallback(void Function(int level, String message) logCallback) {
+    _config.enableLogCallback(logCallback);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_video_sharing/video_info.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:transparent_image/transparent_image.dart';
 
+import 'apis/encoding_provider.dart';
 import 'chewie_player.dart';
 import 'apis/firebase_provider.dart';
-import 'apis/publitio_provider.dart';
-
+import 'package:path/path.dart' as p;
 
 void main() => runApp(MyApp());
 
@@ -39,16 +40,76 @@ class _MyHomePageState extends State<MyHomePage> {
   List<VideoInfo> _videos = <VideoInfo>[];
   bool _imagePickerActive = false;
   bool _uploading = false;
+  bool _canceled = false;
+  double _progress = 0.0;
+  double _videoDuration = 0.0;
 
   @override
   void initState() {
-    PublitioProvider.configurePublitio();
     FirebaseProvider.listenToVideos((newVideos) {
       setState(() {
         _videos = newVideos;
       });
     });
+
+    EncodingProvider.enableStatisticsCallback((int time,
+        int size,
+        double bitrate,
+        double speed,
+        int videoFrameNumber,
+        double videoQuality,
+        double videoFps) {
+      if (_canceled) return;
+
+      setState(() {
+        _progress = time / _videoDuration;
+      });
+    });
+
     super.initState();
+  }
+
+  void onUploadProgress(event) {
+    if (event.type == StorageTaskEventType.progress) {
+      final double progress =
+          event.snapshot.bytesTransferred / event.snapshot.totalByteCount;
+      setState(() {
+        _progress = progress;
+      });
+    }
+  }
+
+  Future<String> _uploadFile(filePath) async {
+    final file = new File(filePath);
+    final basename = p.basename(filePath);
+
+    final StorageReference ref = FirebaseStorage.instance.ref().child(basename);
+    StorageUploadTask uploadTask = ref.putFile(file);
+    uploadTask.events.listen(onUploadProgress);
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+    String videoUrl = await taskSnapshot.ref.getDownloadURL();
+    return videoUrl;
+  }
+
+  Future<void> _processVideo(rawVideoPath) async {
+    final thumbFilePath =
+        await EncodingProvider.getThumb(rawVideoPath, 100, 200);
+
+    final thumbUrl = await _uploadFile(thumbFilePath);
+
+    final encodedVideoPath = await EncodingProvider.encode(rawVideoPath);
+
+    final aspectRatio = await EncodingProvider.getAspectRatio(encodedVideoPath);
+
+    final videoUrl = await _uploadFile(encodedVideoPath);
+
+    final videoInfo = VideoInfo(
+      videoUrl: videoUrl,
+      thumbUrl: thumbUrl,
+      coverUrl: thumbUrl,
+      aspectRatio: aspectRatio,
+    );
+    await FirebaseProvider.saveVideo(videoInfo);
   }
 
   void _takeVideo() async {
@@ -66,8 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
 
     try {
-      final video = await PublitioProvider.uploadVideo(videoFile);
-      await FirebaseProvider.saveVideo(video);
+      _processVideo(videoFile);
     } on PlatformException catch (e) {
       print('${e.code}: ${e.message}');
       //result = 'Platform Exception: ${e.code} ${e.details}';
